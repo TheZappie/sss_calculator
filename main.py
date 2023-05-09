7b6cbc9b940d268291f1ce53d6288a83b25a4930
Savenije, Victor <v.savenije@fugro.com>
2023-05-09 15:35:07 +0200

    nearfield effects added

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ from matplotlib.patches import Polygon
 from matplotlib.collections import LineCollection, CircleCollection, PatchCollection, PolyCollection
 
 
+# resolution_from_manual [range, nominal_frequency]
+resolution_from_manual = {'EdgeTech 4200-MP': {
+    100:  (200, 5),
+    300: (150, 1.3),
+    400: (100, 0.6),
+    600: (100, 0.45),
+    900:  (50, 0.18),
+}}
+
+
 def mirrored_interval(step: float, n: int):
     array = np.arange(0, n * step, step)
     return array - (array[-1] / 2)
@@ -17,27 +27,32 @@ def mirrored_interval(step: float, n: int):
 
 def main():
     st.title("Side-scan resolution calculator")
-    ground_range, vessel_speed, beam_width, frequency = get_input()
+    ground_range, vessel_speed, beam_width, frequency, mode, instrument = get_input()
     sound_velocity = 1500
     n_sensor_positions = 7
     altitude_percentage = 10
     altitude = altitude_percentage / 100 * ground_range
     hypothenusa = math.sqrt(altitude ** 2 + ground_range ** 2)
+    if mode == 'HSM':
+        ping_rate = sound_velocity / (2 * hypothenusa) * 2
+    elif mode == 'HDM':
+        ping_rate = sound_velocity / (2 * hypothenusa)
 
-    ping_rate = sound_velocity / (2 * hypothenusa)
     ping_interval = vessel_speed / ping_rate
     sensor_y = mirrored_interval(ping_interval, n_sensor_positions)
     centers = [np.array([0, y]) for y in sensor_y]
     beam_width_m = math.tan(math.radians(beam_width)) * ground_range
-    array_length = 0.4 # [m]
-    nearfield_range = array_length**2 / (4 * sound_velocity / frequency / 1000)# D**2
+    array_length = 0.4  # [m]
+    nearfield_range = array_length ** 2 / (4 * sound_velocity / frequency / 1000)  # D**2
     # D: length
     # F: frequency
     # C: sound velocity
     nadir_range = 2
     v = 0.5 * array_length
-    right_polygons = [Polygon([(nearfield_range, y - v), (nadir_range, y - v), (nadir_range, y + v), (nearfield_range, y + v), (ground_range, y + beam_width_m), (ground_range, y - beam_width_m)],
-                              alpha=0.2) for y in sensor_y]
+    right_polygons = [Polygon(
+        [(nearfield_range, y - v), (nadir_range, y - v), (nadir_range, y + v), (nearfield_range, y + v),
+         (ground_range, y + beam_width_m), (ground_range, y - beam_width_m)],
+        alpha=0.2) for y in sensor_y]
     left_polygons = [Polygon([(0, y), (-ground_range, y + beam_width_m), (-ground_range, y - beam_width_m)],
                              alpha=0.2) for y in sensor_y]
     fig, ax = plt.subplots()
@@ -67,32 +82,42 @@ def main():
 
     fig2, ax2 = plt.subplots()
     lc = LineCollection([[(0, v), (nearfield_range, v), (ground_range, resolution)]], linewidth=3.0, )
-    ax2.axis([0, ground_range + 10, 0, resolution*1.1])
+    ax2.axis([0, ground_range + 10, 0, resolution * 1.1])
     ax2.add_collection(lc)
+    v = resolution_from_manual.get(instrument).get(frequency)
+    if v:
+        ax2.scatter([v[0]], [v[1]], label='According to Edgetech manual')
     ax2.set_title("Resolution over distance")
+    ax2.legend()
     ax2.set_xlabel("Cross-track distance [m]")
     ax2.set_ylabel("Across track resolution [m]")
     st.pyplot(fig2)
 
 
 def get_input():
-    range = st.slider("(ground) Range (per channel) [m]", 0.0, 200.0, 70.0, step=0.5)
+    range = st.slider("(ground) Range (per channel) [m]", 10.0, 200.0, 70.0, step=0.5)
     speed = st.slider("Vessel speed [m/s]", 0.0, 10.0, 3.0, step=0.1)
 
-    data = {'EdgeTech 4200-MP HDM': {100: 0.64, 300: 0.28, 400: 0.3, 600: 0.26, 900: 0.2},
-            'EdgeTech 4200-MP HSM': {100: 1.26, 300: 0.54, 400: 0.4, 600: 0.34, 900: 0.3},
-            'Edgetech 4125': {400: 0.46, 600: 0.33, 900: 0.28, 1600: 0.2},
-            'Edgetech 4205 HDM': {120: 0.7, 230: 0.44, 410: 0.28, 540: 0.26, 850: 0.23},
-            'Edgetech 4205 HDM HSM/MP': {120: 0.95, 230: 0.63, 410: 0.38, 540: 0.35, 850: 0.3},
+    data = {'EdgeTech 4200-MP': {'HDM': {100: 0.64, 300: 0.28, 400: 0.3, 600: 0.26, 900: 0.2},
+                                 'HSM': {100: 1.26, 300: 0.54, 400: 0.4, 600: 0.34, 900: 0.3}
+                                 },
+            'Edgetech 4125': {'HDM': {400: 0.46, 600: 0.33, 900: 0.28, 1600: 0.2}},
+            'Edgetech 4205': {'HDM': {120: 0.7, 230: 0.44, 410: 0.28, 540: 0.26, 850: 0.23},
+                              'HSM': {120: 0.95, 230: 0.63, 410: 0.38, 540: 0.35, 850: 0.3}},
             }
-
     instrument = st.sidebar.selectbox("Instrument", data.keys())
-    freq = st.sidebar.select_slider("Frequency", data[instrument])
-    beam_width = data[instrument][freq]
-    st.sidebar.text(f"Beamwidth of chosen configuration: {beam_width:.2f}°")
-    st.sidebar.text(f"sources: https://www.edgetech.com/wp-content/uploads/2019/07/app_note_beamwidth.pdf")
-    return range, speed, beam_width, freq
+    mode = st.sidebar.selectbox("Mode", data[instrument])
+    freq = st.sidebar.selectbox("Frequency", data[instrument][mode])
+    beam_width = data[instrument][mode][freq]
+    st.sidebar.text(f"Beamwidth of chosen configuration: \n{beam_width:.2f}°")
 
+    st.sidebar.text(f"data from :\n"
+                    f"https://www.edgetech.com/wp-content/uploads/2019/07/0004842_Rev_P.pdf")
+
+    st.sidebar.text(f"sources from https://www.edgetech.com/resource-center/: \n"
+                    f"https://www.edgetech.com/wp-content/uploads/2019/07/app_note_beamwidth.pdf \n"
+                    f"https://www.edgetech.com/wp-content/uploads/2020/10/Feature-detection-Rev3.pdf")
+    return range, speed, beam_width, freq, mode, instrument
 
 
 if __name__ == '__main__':
